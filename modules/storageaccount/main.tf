14f0f0fc9e82f03629d563078011738667823f5f
RaquelMostaza <94902262+RaquelMostaza@users.noreply.github.com>
2023-06-02 17:08:28 +0200

    Add files via upload

--- modules/storageaccount/main.tf
+++ modules/storageaccount/main.tf
@@ -1,8 +1,175 @@
-## Create SA and container that will store the glossary exports
-resource "azurerm_storage_account" "example23" {
-  name                     = "fsldadfl"
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+data "azurerm_client_config" "current" {}
+
+data "http" "ip" {
+  url = "https://ifconfig.me"
+}
+
+locals {
+  safe_prefix  = replace(var.prefix, "-", "")
+  safe_postfix = replace(var.postfix, "-", "")
+}
+
+data "azurerm_storage_account" "st" {
+  name                     = "st${local.safe_prefix}${local.safe_postfix}${var.env}"
+  resource_group_name      = var.rg_name
+}
+data "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+}
+
+data "azurerm_virtual_network" "vnet_default" {
+  name                = var.vnet_name
+  resource_group_name = var.vnet_rg
+}
+
+# Subnets
+data "azurerm_subnet" "snet_default" {
+  name                                           = var.subnet_name
+  resource_group_name                            = var.vnet_rg
+  virtual_network_name                           = data.azurerm_virtual_network.vnet_default.name
+}
+
+/*
+resource "azurerm_storage_account_network_rules" "firewall_rules" {
+  storage_account_id = data.azurerm_storage_account.st.id
+  default_action             = "Deny"
+}*/
+
+# Virtual Network & Firewall configuration
+resource "azurerm_storage_account_network_rules" "firewall_rules" {
+  storage_account_id = data.azurerm_storage_account.st.id
+
+  default_action             = "Deny"
+  ip_rules                   = var.ip_ranges #[data.http.ip.body] # [data.http.ip.body]
+  #Removal of vnets as a fix for online endpoints
+  #virtual_network_subnet_ids = [data.azurerm_subnet.snet_default.id]
+  bypass                     = ["AzureServices","Logging","Metrics"]
+  #dynamic "private_link_access" {
+  #  for_each = var.private_link_access
+  #  content {
+  #    endpoint_resource_id = private_link_access.value
+  #  }
+  #}
+}
+
+# DNS Zones
+resource "azurerm_private_dns_zone" "st_zone_blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.rg_name
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+}
+
+resource "azurerm_private_dns_zone" "st_zone_file" {
+  name                = "privatelink.file.core.windows.net"
+  resource_group_name = var.rg_name
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+}
+
+resource "azurerm_private_dns_zone" "st_zone_web" {
+  name                = "privatelink.web.core.windows.net"
+  resource_group_name = var.rg_name
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+}
+
+# Linking of DNS zones to Virtual Network
+
+resource "azurerm_private_dns_zone_virtual_network_link" "st_zone_link_blob" {
+  name                  = "${var.prefix}${var.postfix}_link_st_blob"
+  resource_group_name   = var.rg_name
+  private_dns_zone_name = azurerm_private_dns_zone.st_zone_blob[0].name
+  virtual_network_id    = data.azurerm_virtual_network.vnet_default.id
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "st_zone_link_file" {
+  name                  = "${var.prefix}${var.postfix}_link_st_file"
+  resource_group_name   = var.rg_name
+  private_dns_zone_name = azurerm_private_dns_zone.st_zone_file[0].name
+  virtual_network_id    = data.azurerm_virtual_network.vnet_default.id
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "st_zone_link_web" {
+  name                  = "${var.prefix}${var.postfix}_link_st_web"
+  resource_group_name   = var.rg_name
+  private_dns_zone_name = azurerm_private_dns_zone.st_zone_web[0].name
+  virtual_network_id    = data.azurerm_virtual_network.vnet_default.id
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+}
+
+# Private Endpoint configuration
+
+resource "azurerm_private_endpoint" "st_pe_blob" {
+  name                = "pe-${data.azurerm_storage_account.st.name}-blob"
+  location            = var.location
+  resource_group_name = var.rg_name
+  subnet_id           = data.azurerm_subnet.snet_default.id
+
+  private_service_connection {
+    name                           = "psc-blob-${var.prefix}-${var.postfix}${var.env}"
+    private_connection_resource_id = data.azurerm_storage_account.st.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group-blob"
+    private_dns_zone_ids = [azurerm_private_dns_zone.st_zone_blob[0].id]
+  }
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+
+  tags = var.tags
+}
+
+resource "azurerm_private_endpoint" "st_pe_file" {
+  name                = "pe-${data.azurerm_storage_account.st.name}-file"
+  location            = var.location
+  resource_group_name = var.rg_name
+  subnet_id           = data.azurerm_subnet.snet_default.id
+
+  private_service_connection {
+    name                           = "psc-file-${var.prefix}-${var.postfix}${var.env}"
+    private_connection_resource_id = data.azurerm_storage_account.st.id
+    subresource_names              = ["file"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group-file"
+    private_dns_zone_ids = [azurerm_private_dns_zone.st_zone_file[0].id]
+  }
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+
+  tags = var.tags
+}
+
+resource "azurerm_private_endpoint" "st_pe_web" {
+  name                = "pe-${data.azurerm_storage_account.st.name}-web"
+  location            = var.location
+  resource_group_name = var.rg_name
+  subnet_id           = data.azurerm_subnet.snet_default.id
+
+  private_service_connection {
+    name                           = "psc-web-${var.prefix}-${var.postfix}${var.env}"
+    private_connection_resource_id = data.azurerm_storage_account.st.id
+    subresource_names              = ["web"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group-web"
+    private_dns_zone_ids = [azurerm_private_dns_zone.st_zone_web[0].id]
+  }
+
+  count = var.enable_aml_secure_workspace ? 1 : 0
+
+  tags = var.tags
 }
--- modules/storageaccount/variables.tf
+++ modules/storageaccount/variables.tf
@@ -1,5 +1,65 @@
+variable "rg_name" {
+  type        = string
+  description = "Resource group name"
+}
+
 variable "location" {
   type        = string
-  default     = "WestEurope"
-  description = "Region to deploy to"
+  description = "Location of the resource group"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "A mapping of tags which should be assigned to the Resource Group"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for the module name"
+}
+
+variable "postfix" {
+  type        = string
+  description = "Postfix for the module name"
+}
+
+variable "env" {
+  type        = string
+  description = "Environment prefix"
+}
+
+variable "hns_enabled" {
+  type        = bool
+  description = "Hierarchical namespaces enabled/disabled"
+  default     = true
+}
+
+variable "ip_ranges" {
+  type    = set(string)
+  default = []
+}
+
+variable "enable_aml_secure_workspace" {
+  description = "Variable to enable or disable AML secure workspace"
+}
+variable "vnet_name" {
+  type        = string
+  description = "Name of the virtual network"
+}
+
+variable "vnet_rg" {
+  type        = string
+  description = "Name of the resource group containing the virtual network"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Name of the subnet within the virtual network"
+}
+
+variable "private_link_access" {
+  type        = list(string)
+  default     = null
+  description = "Define a list of endpoint id's to whitelist on network rules"
 }
